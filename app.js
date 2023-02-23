27ea777e34de37c9832f2ef3bdd4e564596b4ca5
gitcraft <gitkhan2021@gmail.com>
2023-02-23 12:56:40 +0100

    buttons were set

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+let color = 'black';
+
 document.addEventListener("DOMContentLoaded", function(){
     crtBrd(16);
+
+    let btnPopup = document.querySelector('#popup');
+    btnPopup.addEventListener('click', function(){
+        let size = getSize();
+        crtBrd(size);
+    })
 })
 
 function crtBrd(size){
@@ -12,8 +20,38 @@ function crtBrd(size){
 
     for(let i = 0; i < numDiv; i++){
         let div = document.createElement('div');
-        div.style.backgroundColor = 'yellow';
+        div.addEventListener('mouseover', colorDiv);
         board.insertAdjacentElement('beforeend', div);
     }
+}
+
+function getSize(){
+    let input = prompt("What will be the size of the board");
+    let message = document.querySelector('#message');
+    if(input == ''){
+        message.innerHTML = 'please provide a number';
+    }else if(input < 0 || input > 100){
+        message.innerHTML = 'Provide number between 1 and 100';
+    }else{
+        message.innerHTML = 'Now you can play';
+        return input;
+    }
+}
+
+function colorDiv(){
+    if(color == 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%`;
+    }else{
+        this.style.backgroundColor = 'black';
+    }
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll('div');
+    divs.forEach((div) => div.style.backgroundColor = 'white');
 }
 
