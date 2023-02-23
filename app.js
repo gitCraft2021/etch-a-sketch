a91ebc7e235842893aad4124e791d69b6ad17aa5
gitcraft <gitkhan2021@gmail.com>
2023-02-23 12:17:10 +0100

    divs were created for board

---
+++ app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+    crtBrd(16);
+})
+
+function crtBrd(size){
+    let board = document.querySelector('.board');
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDiv = size * size;
+
+    for(let i = 0; i < numDiv; i++){
+        let div = document.createElement('div');
+        div.style.backgroundColor = 'yellow';
+        board.insertAdjacentElement('beforeend', div);
+    }
+}
+
