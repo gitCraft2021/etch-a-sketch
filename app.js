013e8e4cfd5f7fe8d9385363057053b73a0a40b0
gitcraft <gitkhan2021@gmail.com>
2023-02-23 13:04:49 +0100

    project is done

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 let color = 'black';
+let click = false;
 
 document.addEventListener("DOMContentLoaded", function(){
-    crtBrd(16);
+    crtBrd(32);
+
+    document.querySelector('body').addEventListener('click', function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector('#draw');
+            if(click){
+                draw.innerHTML = "Now You Can Draw"
+            }else{
+                draw.innerHTML = "You're Not Allowed to Draw"
+            }
+        }
+    })
 
     let btnPopup = document.querySelector('#popup');
     btnPopup.addEventListener('click', function(){
@@ -39,12 +52,15 @@ function getSize(){
 }
 
 function colorDiv(){
-    if(color == 'random'){
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%`;
-    }else{
-        this.style.backgroundColor = 'black';
-    }
+    if(click){
+        if(color == 'random'){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%`;
+        }else{
+            this.style.backgroundColor = 'black';
+        }
 }
+    }
+     
 
 function setColor(colorChoice){
     color = colorChoice;
